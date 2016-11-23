62e1b84778f7f8f4f09819e02ae0b06d5803c5d6
Zane H <zaxa13@gmail.com>
2016-11-23 19:55:21 +0000

    Built scraper that creates a dataDoc for a module page containing all fields of data.  Needs testing.

--- src/main/java/webscraper/ModuleScraper.java
+++ src/main/java/webscraper/ModuleScraper.java
@@ -1,6 +1,7 @@
 package webscraper;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class ModuleScraper extends PageScraper {
 
@@ -14,10 +15,20 @@ public class ModuleScraper extends PageScraper {
 	@Override
 	public DataDoc scrapeDocument(Document doc) {
 		this.doc = doc;
-				
+		Elements paras = doc.select("p");
+		
+		for(int i = 0; i < paras.size(); i++){
+			System.out.println(paras.get(i).text());
+		}
+		
+		for(int i = 0; i < paras.size(); i++){
+			String fieldData[] = paras.get(i).text().split(":");
+			if(paras.get(i).text().split(":").length == 2){
+				dataDoc.addField(fieldData[0], fieldData[1]);
+			}
+		}
+		
 		return dataDoc;
 	}
 
-	
-	
 }
