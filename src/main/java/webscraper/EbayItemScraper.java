e3a1c1439745181b5954fa0058741ca8caac145e
psynt <psynt@nottingham.ac.uk>
2016-12-15 17:45:06 +0000

    Scraper can now cope with missing information

--- src/main/java/webscraper/EbayItemScraper.java
+++ src/main/java/webscraper/EbayItemScraper.java
@@ -1,20 +1,20 @@
 package webscraper;
 
-/**
- * Scraper for Ebay Item Webpages.  Loads all data into a Ttem object.
- * @author zane
- *
- */
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import content.Attribute;
 import content.Item;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+/**
+ * Scraper for Ebay Item Webpages.  Loads all data into a Ttem object.
+ * @author zane
+ *
+ */
 public class EbayItemScraper extends PageScraper {
+
+	public static final int NUM_FIELDS = 5;
+	public static final String NOT_FOUND = "Not mentioned";
 	
 	public EbayItemScraper(Document doc) {
 		super(doc);
@@ -24,40 +24,62 @@ public class EbayItemScraper extends PageScraper {
 	@Override
 	public Item scrapeDocument() {
 		
-		addName();
-		addCondition();
-		addPrice();
-		addRemainingTime();
-		addShipping();
+		item.addAttribute(getName());
+		item.addAttribute(getCondition());
+		item.addAttribute(getPrice());
+		item.addAttribute(getRemainingTime());
+		item.addAttribute(getShipping());
 		
 		return item;
 	}
 
-	private void addName() {
-		Element name = doc.getElementById("itemTitle");
-		String itemName = name.text().substring(15);
-		item.addAttribute(new Attribute<String>("name", itemName));
+	//functionality for multi-listings to be added later
+
+	private Attribute<String> getName() {
+		try {
+			Element name = doc.getElementById("itemTitle");
+			String itemName = name.text().substring(15);
+			return new Attribute<String>("name", itemName);
+		} catch (NullPointerException e) {
+			return new Attribute<String>("name", NOT_FOUND);
+		}
 	}
 
 
-	private void addCondition() {
-		Element condition = doc.getElementById("vi-itm-cond");
-		item.addAttribute(new Attribute<String>("condition", condition.text()));
+	private Attribute<String> getCondition() {
+		try{
+			Element condition = doc.getElementById("vi-itm-cond");
+			return new Attribute<String>("condition", condition.text());
+		} catch (NullPointerException e) {
+			return new Attribute<String>("condition", NOT_FOUND);
+		}
 	}
 	
-	private void addPrice() {
-		Element price = doc.getElementById("vi-mskumap-none");
-		item.addAttribute(new Attribute<String>("price", price.text()));
+	private Attribute<String> getPrice() {
+		try{
+			Element price = doc.getElementById("vi-mskumap-none");
+			return new Attribute<String>("price", price.text());
+		} catch (NullPointerException e) {
+			return new Attribute<String>("condition", NOT_FOUND);
+		}
 	}
 	
-	private void addRemainingTime() {
-		Element remainingTime = doc.getElementById("vi-cdown_timeLeft");
-		item.addAttribute(new Attribute<String>("remaining time", remainingTime.text()));
+	private Attribute<String> getRemainingTime() {
+		try{
+			Element remainingTime = doc.getElementById("vi-cdown_timeLeft");
+			return new Attribute<String>("remaining time", remainingTime.text());
+		} catch (NullPointerException e) {
+			return new Attribute<String>("condition", NOT_FOUND);
+		}
 	}
 	
-	private void addShipping() {
-		Element shipping = doc.getElementById("fShippingSvc");
-		item.addAttribute(new Attribute<String>("shipping", shipping.text()));
+	private Attribute<String> getShipping() {
+		try{
+			Element shipping = doc.getElementById("fShippingSvc");
+			return new Attribute<String>("shipping", shipping.text());
+		} catch (NullPointerException e) {
+			return new Attribute<String>("shipping", NOT_FOUND);
+		}
 	}
 
 }
