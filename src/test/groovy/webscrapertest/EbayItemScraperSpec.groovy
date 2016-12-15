b2cb7bfad3c843986078ea1eb9cc99ee5a89a943
psynt <psynt@nottingham.ac.uk>
2016-12-15 17:45:36 +0000

    Added test for Ebay Item scraper

---
+++ src/test/groovy/webscrapertest/EbayItemScraperSpec.groovy
@@ -0,0 +1,32 @@
+package webscrapertest
+
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+import webscraper.DocumentLoader
+import webscraper.EbayItemScraper
+
+/**
+ * Created by nichita on 15.12.2016.
+ */
+class EbayItemScraperSpec extends Specification {
+
+    @Unroll
+    def "Test sttributes returned (for single item listings) URL: #testURL .html"(){
+        given:
+        @Subject
+        def sc = new EbayItemScraper(DocumentLoader.load(this.getClass().getResource("/" + testURL + ".html").toURI()))
+
+        when:
+        def it = sc.scrapeDocument();
+
+        then:
+        it.getAttributes().size() == EbayItemScraper.NUM_FIELDS;
+
+        where:
+        testURL << ["ebayItem"]
+
+
+    }
+
+}
--- src/test/groovy/webscrapertest/EbayResultScraperSpec.groovy
+++ src/test/groovy/webscrapertest/EbayResultScraperSpec.groovy
@@ -1,18 +1,27 @@
 package webscrapertest
 
 import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
 import webscraper.DocumentLoader
 import webscraper.EbayResultScraper
 /**
  * Created by nichita on 15.12.2016.
  */
 class EbayResultScraperSpec extends Specification {
+
+    @Unroll("URL: #testURL .html")
     def "Test number of links returned"(){
         given:
-        EbayResultScraper sc = new EbayResultScraper(DocumentLoader.load(this.getClass().getResource("/ebaySearch.html").toURI()))
+        @Subject
+        def sc = new EbayResultScraper(DocumentLoader.load(this.getClass().getResource("/" + testURL + ".html").toURI()))
 
         expect:
-        sc.scrapeLinks().size() == 1
+        sc.scrapeLinks().size() == expectedOutput
+
+        where:
+        testURL             ||   expectedOutput
+        "ebaySearch"   ||  1
 
 
     }
