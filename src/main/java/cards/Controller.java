34b0b45efb18abcdb82f89acce307512aa78ca1c
psyegb <psyegb@exmail.nottingham.ac.uk>
2017-02-15 15:25:35 +0000

    working ebayCard / moduleCard layout

---
+++ src/main/java/cards/Controller.java
@@ -0,0 +1,44 @@
+package cards;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class Controller {
+
+
+
+    @FXML Pane mainPane;
+    @FXML VBox cardStackLeft;
+    @FXML VBox cardStackRight;
+
+
+
+    @FXML
+    public void initialize(){
+
+        // Creating a card for each column of the two vbox's
+        ebayCard c1 = new ebayCard("01","12.00","good","2 hours", "12.00");
+        ebayCard c2 = new ebayCard("01","12.00","good","2 hours", "12.00");
+        ebayCard c3 = new ebayCard("01","12.00","good","2 hours", "12.00");
+        ebayCard c4 = new ebayCard("01","12.00","good","2 hours", "12.00");
+        ebayCard c5 = new ebayCard("01","12.00","good","2 hours", "12.00");
+        ebayCard c6 = new ebayCard("01","12.00","good","2 hours", "12.00");*/
+
+        //add the left cards to the left vbox
+        cardStackLeft.getChildren().add(0,c1);
+        cardStackLeft.getChildren().add(1,c2);
+        cardStackLeft.getChildren().add(2,c3);
+
+        //add the right cards to the right vbox
+        cardStackRight.getChildren().add(0,c4);
+        cardStackRight.getChildren().add(1,c5);
+        cardStackRight.getChildren().add(2,c6);
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/cards/moduleCard.java
@@ -0,0 +1,97 @@
+package cards;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+
+//The 'card'
+public class moduleCard extends TabPane {
+
+    private String moduleName ;
+    private String moduleCode ;
+    private String preRequisites ;
+    private String numCredits ;
+    private String moduleSemester ;
+
+    private Label name = new Label();
+    private Label code = new Label();
+    private Label preReqs = new Label();
+    private Label credits = new Label();
+    private Label semester = new Label();
+
+    public moduleCard(String moduleName, String moduleCode, String  preRequisites, String numCredits, String moduleSemester){
+
+
+        super();
+
+        super.setPrefSize(250,200);
+
+        this.moduleName= moduleName;
+        this.moduleCode = moduleCode;
+        this.preRequisites = preRequisites;
+        this.numCredits = numCredits;
+        this.moduleSemester = moduleSemester;
+
+        name.setText(getModuleName());
+        code.setText(getModuleCode());
+        preReqs.setText(getPreRequisites());
+        credits.setText(getNumCredits());
+        semester.setText(getModuleSemester());
+
+        Tab tab = new Tab();
+        tab.setClosable(false);
+
+        tab.setText(getModuleName());
+        VBox tabBox = new VBox();
+        tabBox.setAlignment(Pos.TOP_CENTER);
+        tabBox.setBackground(new Background(new BackgroundFill(Color.LIGHTSTEELBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        tabBox.getChildren().addAll(
+            name,
+            code,
+            preReqs,
+            credits,
+            semester
+        );
+
+        tab.setContent(tabBox);
+
+
+        super.getTabs().add(tab);
+
+    }
+
+
+    public String getModuleName(){
+
+        return moduleName;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public String getModuleSemester() {
+        return moduleSemester;
+    }
+
+    public String getNumCredits() {
+        return numCredits;
+    }
+
+    public String getPreRequisites() {
+        return preRequisites;
+    }
+
+
+
+
+}
