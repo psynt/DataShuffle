1655438a1a17ad2d34a70fafbfdc9d142ce36d16
ADAM LEI <psykl1@exmail.nottingham.ac.uk>
2017-03-30 17:59:49 +0100

    New splash screen

--- src/main/java/splash/Controller.java
+++ src/main/java/splash/Controller.java
@@ -12,17 +12,17 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
-import javafx.scene.control.Slider;
+
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+
 import javafx.scene.layout.GridPane;
 
 
